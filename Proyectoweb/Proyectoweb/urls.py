54a24118ea23f4ab1807dcafb0bb389426cab8d2
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-07-28 22:27:37 -0500

    creando vistas y url
    creando vistas y url

--- Proyectoweb/Proyectoweb/urls.py
+++ Proyectoweb/Proyectoweb/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from ProyectoWebApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name="Home"),
+    path('servicios',views.servicios, name="Servicios"),
+    path('tienda',views.tienda, name="Tienda"),
+    path('contacto',views.contacto, name="Contacto"),
+
+
+
 ]
