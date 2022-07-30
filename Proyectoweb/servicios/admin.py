9d707a74b2bf4c65253765e1c235aba083db37a7
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-07-30 17:55:54 -0500

    Se creó el superusuario del panel de administración, se implemento  la app servicio al panel de administración.
    Se creó el superusuario del panel de administración, se implemento  la app servicio al panel de administración.

--- Proyectoweb/servicios/admin.py
+++ Proyectoweb/servicios/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+
+from .models import Servicios
+
 # Register your models here.
+
+
+class ServicioAdmin(admin.ModelAdmin):
+    
+   readonly_fields=('created','update')
+
+
+admin.site.register(Servicios,ServicioAdmin)
+
+
+
+
