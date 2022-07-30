8a90cd445226137a3901411f29397ec9e30655b5
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-07-29 21:22:27 -0500

    Se creó la app servicios
    Se creó la app servicios

---
+++ Proyectoweb/servicios/models.py
@@ -0,0 +1,20 @@
+from email.headerregistry import ContentDispositionHeader
+from tabnanny import verbose
+from tkinter import image_names
+from django.db import models
+
+# Create your models here.
+
+class Servicios(models.Model):
+    titulo=models.CharField(max_length=50)
+    contenido=models.CharField(max_length=50)
+    imagen=models.ImageField()
+    created=models.DateTimeField(auto_now_add=True)
+    update=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='servicio'
+        verbose_name_plural='servicios'
+
+    def __str__(self):
+        return self.titulo
