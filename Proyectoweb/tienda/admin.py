062936349c0bf2e1195630086a1b64965da49822
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-08-03 14:58:39 -0500

    Se creó el template de la app compra y se agregó al admin de django  ya está completa la gestión de la tienda.
    Se creó el template de la app compra y se agregó al admin de django  ya está completa la gestión de la tienda.

--- Proyectoweb/tienda/admin.py
+++ Proyectoweb/tienda/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import CategoriaProd,Producto
+
 # Register your models here.
+
+
+class CategoriaProAdmin(admin.ModelAdmin):
+   readonly_fields=('created','update')
+
+class ProductoaAdmin(admin.ModelAdmin):
+   readonly_fields=('created','update')
+
+
+
+
+admin.site.register(CategoriaProd,CategoriaProAdmin)
+admin.site.register(Producto,ProductoaAdmin)
